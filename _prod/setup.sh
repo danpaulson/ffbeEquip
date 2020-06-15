8eb706e51f316635fe49090bf2bd0ff72590eca7
Dan Paulson <danpaulson@gmail.com>
2020-06-15 10:52:02 -0500

    Remove varnish-setup

--- _prod/setup.sh
+++ _prod/setup.sh
@@ -6,7 +6,7 @@ apt-get install varnish nginx
 # Varnish
 service varnish stop
 
-cp /etc/varnish/default.vcl /etc/varnish/default.vcl.org
+mv /etc/varnish/default.vcl /etc/varnish/default.vcl.org
 ln -s $REPO_DIR/_prod/varnish.vcl /etc/varnish/default.vcl
 ln -s $REPO_DIR/_prod/varnish.service /etc/systemd/system/varnish.service
 
