a4ceb19d6d00c7f1416937f5c88eab8439dd203f
Dan Paulson <danpaulson@gmail.com>
2020-06-15 11:22:51 -0500

    Update setup to copy nginx conf

--- _prod/setup.sh
+++ _prod/setup.sh
@@ -1,6 +1,8 @@
 REPO_DIR="/srv/web/ffbe"
+REPO_NAME="ffbe"
 
 # Install Nginx / Varnish
+curl -s https://packagecloud.io/install/repositories/varnishcache/varnish60lts/script.deb.sh | sudo bash
 apt-get install varnish nginx
 
 # Varnish
@@ -9,6 +11,9 @@ service varnish stop
 mv /etc/varnish/default.vcl /etc/varnish/default.vcl.org
 ln -s $REPO_DIR/_prod/varnish.vcl /etc/varnish/default.vcl
 ln -s $REPO_DIR/_prod/varnish.service /etc/systemd/system/varnish.service
+
+# Nginx
+ln -s $REPO_DIR/_prod/nginx.conf /etc/nginx/sites-enabled/$REPO_NAME
 
 systemctl daemon-reload
 service varnish start
